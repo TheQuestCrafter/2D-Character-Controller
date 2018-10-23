597e7d4e3089a969b61ca07bb57eb21e78acb472
TheQuestCrafter <thequestcrafter@gmail.com>
2018-10-22 23:36:30 -0500

    Added Movement and Walking/Idle Animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField]
+    float maxSpeed = 10f;
+    bool facingRight = true;
+    [SerializeField]
+    Rigidbody2D rb2d;
+    [SerializeField]
+    Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float move = Input.GetAxis("Horizontal");
+        rb2d.AddForce(Vector2.right * move * maxSpeed);
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        
+        //rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+        //didn't move character at all
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+	}
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
